82a0cebfcb64596be44cfaae761a1e98e59b0135
marcusteixeirabr <marcusteixeirabr@gmail.com>
2026-02-21 16:38:11 +0000

    Adiciona o esqueleto do HtmlParser para futuras implementações de parsing de HTML.

---
+++ app/src/test/java/br.dev.marcus.praticagem/HtmlParser.java
@@ -0,0 +1,8 @@
+package br.dev.marcus.praticagem;
+
+public class HtmlParser {
+
+    public List<MovimentacaoDTO> parse(String html) {
+        throw new UnsupportedOperationException("Ainda não implementado");
+    }
+}
